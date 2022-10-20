5810d1a23331c1497ef39e0abc4db3b00f2de675
os-ksharshembie <kadyrbek.sharshembiev@gmail.com>
2022-10-20 20:12:26 +0600

    ShowNewCount logic changed:
    if count equal 10 then show Toast Congradulations
    if count equal 15 then change text color to green

--- app/src/main/java/com/ksharshembie/counter/MainActivity.kt
+++ app/src/main/java/com/ksharshembie/counter/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.ksharshembie.counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.ksharshembie.counter.databinding.ActivityMainBinding
 import com.ksharshembie.counter.presenter.CounterPresenter
 import com.ksharshembie.counter.view.CounterView
-import kotlinx.coroutines.processNextEventInCurrentThread
 
 class MainActivity : AppCompatActivity(), CounterView {
     lateinit var binding: ActivityMainBinding
@@ -13,14 +13,14 @@ class MainActivity : AppCompatActivity(), CounterView {
     private val presenter = CounterPresenter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         presenter.attachView(this)
         initClickers()
     }
 
     private fun initClickers() {
-        with(binding){
+        with(binding) {
             btnIncrement.setOnClickListener {
                 presenter.increment()
             }
@@ -31,7 +31,22 @@ class MainActivity : AppCompatActivity(), CounterView {
     }
 
     override fun showNewCount(count: Int) {
-        binding.tvResult.text = count.toString()
+
+        if (count == 10) {
+            binding.tvResult.text = count.toString()
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.congragulations),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else if (count == 15) {
+            binding.tvResult.setTextColor(getColor(R.color.green))
+            binding.tvResult.text = count.toString()
+        } else {
+            binding.tvResult.setTextColor(getColor(R.color.purple_500))
+            binding.tvResult.text = count.toString()
+        }
+
     }
 
 
